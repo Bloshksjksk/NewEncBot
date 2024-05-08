987f55b548b8a16971623fff910592c1d9d53fba
Bloshksjksk <kind.proud.dentist@ghostlymail.com>
2024-05-08 11:47:44 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -17,7 +17,7 @@ class Database:
             caption=None,
             thumbnail=None,
             ffmpegcode=None,
-            metadata=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Kdramaland" -metadata author="@Snowball_Official" -metadata:s:s title="Subtitled By :- @Kdramaland" -metadata:s:a title="By :- @Kdramaland" -metadata:s:v title="By:- @Snowball_Official" """,
+            metadata=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @fligher" -metadata author="@movie_time_botonly" -metadata:s:s title="Subtitled By :- @movie_time_botonly" -metadata:s:a title="By :- @movie_time_botonly" -metadata:s:v title="By:- @movie_time_botonly" """,
             ban_status=dict(
                 is_banned=False,
                 ban_duration=0,
